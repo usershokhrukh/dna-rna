82ca320e710a5a01f3024a379bb12952e94fea76
usershokhrukh <asxurovsit@gmail.com>
2026-01-03 16:20:43 +0500

    file structures and 30% js

---
+++ index.js
@@ -0,0 +1,6 @@
+const elForm = document.querySelector(".section__form");
+const elLabel = document.querySelector(".section__label");
+elForm.addEventListener("submit", (e)=> {
+  e.preventDefault();
+
+})
